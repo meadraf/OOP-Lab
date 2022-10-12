ea01af7e38f3ba0415661eeb28af70c99e5ca359
Anton Manikin <anton.manikin@gmail.com>
2022-10-12 23:45:57 +0300

    added Grass logic

---
+++ OOPLAB/GameObjects/Grass.cs
@@ -0,0 +1,32 @@
+namespace OOPLAB;
+
+public class Grass
+{
+    private bool _isGrown;
+    private int _growRate;
+    
+    public Grass()
+    {
+        _isGrown = true;
+        _growRate = 10;
+        Simulation.Update += Grow;
+    }
+    
+    public void Eaten()
+    {
+        _isGrown = false;
+        _growRate = 0;
+    }
+    
+    private void Grow()
+    {
+        if (_isGrown == false)
+        {
+            _growRate++;
+            if (_growRate == 10)
+            {
+                _isGrown = true;
+            }
+        }
+    }
+}
