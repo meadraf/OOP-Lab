0278b00fb204363c185c1e9d775b03b64c7cd6f8
Anton Manikin <anton.manikin@gmail.com>
2022-10-14 16:22:05 +0300

    Simulation refactoring

--- OOPLAB/Simulation/Simulation.cs
+++ OOPLAB/Simulation/Simulation.cs
@@ -2,31 +2,29 @@ namespace OOPLAB;
 
 class Simulation
 {
-    public int MoveCount { get; private set;}
-    public Statistics Statistics;
+    private readonly Statistics _statistics;
     public static event Action Update;
-    private int _delay;
-    private int _maxTurns;
+    private readonly int _delay;
+    private readonly int _maxTurns;
     
     public Simulation(List<GameObject>[,] map)
     {
-        MoveCount = 0;
         _delay = 700;
         _maxTurns = 100;
-        this.Statistics = new Statistics(map);
+        _statistics = new Statistics(map);
     }
 
     public void Start()
     {
-        while (MoveCount < _maxTurns)
+        while (_statistics.TurnsCount < _maxTurns)
         {
             Update.Invoke();
             Thread.Sleep(_delay);
             
             Console.Clear();
-            Statistics.CountPredators();
-            Statistics.CountPreys();
-            Statistics.Print();
+            _statistics.CountPredators();
+            _statistics.CountPreys();
+            _statistics.Print();
         }
     }
 }
