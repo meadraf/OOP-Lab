0c9b8a8b0ad90f2f47b40bbdf25fd9a230e5f530
Anton Manikin <anton.manikin@gmail.com>
2022-10-19 16:05:47 +0300

    added every animal count output

--- OOPLAB/Simulation/Statistics.cs
+++ OOPLAB/Simulation/Statistics.cs
@@ -7,11 +7,12 @@ class Statistics
     {
         get => _animalsCount = PredatorsCount + PreysCount;
         private set => _animalsCount = value;
-
     }
     public int PredatorsCount { get; private set; }
     public int PreysCount { get; private set; }
     public int TurnsCount { get; private set; }
+    public Dictionary<Type, int> PreySpecietyCounter;
+    public Dictionary<Type, int> PredatorSpecietyCounter;
     private readonly List<GameObject>[,] _map;
 
     public Statistics(List<GameObject>[,] map)
@@ -22,19 +23,24 @@ class Statistics
         PredatorsCount = 0;
         PreysCount = 0;
         TurnsCount = 0;
+        PreySpecietyCounter = new Dictionary<Type, int>(4);
+        PredatorSpecietyCounter = new Dictionary<Type, int>(4);
+
     }
 
     private void NextTurn() => TurnsCount++;
     
     public int CountPredators()
     {
-        PredatorsCount = 0;
+        ResetPredators();
         foreach (var cell in _map)
         {
             foreach (var animal in cell)
             {
                 if (animal is Predators)
                 {
+                    PredatorSpecietyCounter.TryAdd(animal.GetType(), 0);
+                    PredatorSpecietyCounter[animal.GetType()]++;
                     PredatorsCount++;
                 }
             }
@@ -45,18 +51,32 @@ class Statistics
 
     public int CountPreys()
     {
-        PreysCount = 0;
+        ResetPreys();
         foreach (var cell in _map)
         {
             foreach (var animal in cell)
             {
                 if (animal is Preys)
                 {
+                    PreySpecietyCounter.TryAdd(animal.GetType(), 0);
+                    PreySpecietyCounter[animal.GetType()]++;
                     PreysCount++;
                 }
             }
         }
 
         return PreysCount;
+    }
+
+    private void ResetPreys()
+    {
+        PreysCount = 0;
+        PreySpecietyCounter.Clear();
+    }
+
+    private void ResetPredators()
+    {
+        PredatorsCount = 0;
+        PredatorSpecietyCounter.Clear();
     }
 }
--- OOPLAB/Simulation/StatisticsOutput.cs
+++ OOPLAB/Simulation/StatisticsOutput.cs
@@ -7,6 +7,17 @@ static class StatisticsOutput
         Console.WriteLine("Turn " + statistics.TurnsCount + ":");
         Console.WriteLine("Number of predators: " + statistics.PredatorsCount);
         Console.WriteLine("Number of preys: " + statistics.PreysCount);
-        Console.WriteLine("\nSum of animals: " + statistics.AnimalsCount);
+        Console.WriteLine("\nSum of animals: " + statistics.AnimalsCount +"\n");
+        
+        Console.WriteLine("Bears: " + statistics.PredatorSpecietyCounter[typeof(Bear)]);
+        Console.WriteLine("Hyenas: " + statistics.PredatorSpecietyCounter[typeof(Hyena)]);
+        Console.WriteLine("Tigers: " + statistics.PredatorSpecietyCounter[typeof(Tiger)]);
+        Console.WriteLine("Wolfs: " + statistics.PredatorSpecietyCounter[typeof(Wolf)]);
+        Console.WriteLine();
+        
+        Console.WriteLine("Bulls: " + statistics.PreySpecietyCounter[typeof(Bull)]);
+        Console.WriteLine("Cows: " + statistics.PreySpecietyCounter[typeof(Cow)]);
+        Console.WriteLine("Rabbits: " + statistics.PreySpecietyCounter[typeof(Rabbit)]);
+        Console.WriteLine("Sheeps: " + statistics.PreySpecietyCounter[typeof(Sheep)]);
     }
 }
